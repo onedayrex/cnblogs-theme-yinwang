8a8b2fdd9afacbcc47392c7b5a9bd36c1c84589b
onedayrex <1115757866@qq.com>
2023-04-12 09:55:50 +0800

    添加评论区优化

--- home.js
+++ home.js
@@ -16,7 +16,7 @@
             return $('<h2/>',{
                 html: this.innerHTML
             });
-        })
+        });
 
         //判断是主页还是文章页面
         function isMainPage(){
@@ -36,6 +36,20 @@
                 var post_link = $(this).find('a').attr('href')
                 window.location.href=post_link
             });
+        }else{
+            //由于评论加载后有其它js处理，设置定时500ms后再执行
+            setTimeout(function(){
+                //评论按钮修改
+                $('#btn_comment_submit').addClass('btn btn-primary')
+                $('#commentbox_opt').find('a').remove()
+                //去掉提示文本
+                var comment_p = $('#comment_form_container').find('p');
+                for(var i=0; i< comment_p.length;i++){
+                    if(comment_p[i].id === undefined || comment_p[i].id === ''){
+                        $(comment_p[i]).remove()
+                    }
+                }
+            },500)
         }
 
     });
